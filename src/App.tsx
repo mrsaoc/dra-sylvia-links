7741e1fd37871cd674e3bf5e11e6f24e84b9a958
mrsaoc <mrsaocwork@gmail.com>
2026-02-19 12:37:16 -0300

    refactor name and image

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ export default function App() {
             {/* --- RODAPÉ SOCIAL --- */}
             <footer className="mt-16 flex flex-col items-center space-y-6 z-10">
                 <div className="flex space-x-6">
-                    <a href="https://instagram.com/sylbravo" target="_blank" rel="noopener noreferrer" className="text-emerald-300 hover:text-amber-300 transition-colors transform hover:scale-110">
+                    <a href="https://www.instagram.com/sylviabravofisio" target="_blank" rel="noopener noreferrer" className="text-emerald-300 hover:text-amber-300 transition-colors transform hover:scale-110">
                         <Instagram className="w-6 h-6" />
                     </a>
                     <a href="#" className="text-emerald-300 hover:text-amber-300 transition-colors transform hover:scale-110">
